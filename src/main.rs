1374325b67d11467c4d937e2b731cda5ee9dd2c5
AlexMiddletonDalby <alex.middletondalby@focusrite.com>
2025-02-20 17:21:33 +0000

    Add more stuff
    Midi things in their own file
    Multiple balls with different octaves
    Basic velocity implementation

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
+mod midi;
+
 use avian2d::math::PI;
 use avian2d::prelude::Restitution;
 use avian2d::prelude::*;
+
 use bevy::prelude::*;
 
+use midir::{MidiOutput, MidiOutputConnection, MidiOutputPort};
+
+#[derive(Resource)]
+struct Midi {
+    output_handle: Option<MidiOutputConnection>,
+}
+
 fn main() {
+    let mut handle: Option<MidiOutputConnection> = None;
+
+    let midi_out = MidiOutput::new("My Test Output").unwrap();
+    let out_ports = midi_out.ports();
+    let port: Option<&MidiOutputPort> = out_ports.get(0);
+    if port.is_some() {
+        println!(
+            "Acquired port {}",
+            midi_out.port_name(port.unwrap()).unwrap()
+        );
+
+        if let Ok(connect_result) = midi_out.connect(port.unwrap(), "test") {
+            handle = Some(connect_result);
+        }
+    }
+
     App::new()
         .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
         .add_systems(Startup, (spawn_ball, setup_pads, setup_camera))
         .add_systems(Update, handle_pad_collisions)
         .insert_resource(Gravity(Vec2::NEG_Y * 700.0))
+        .insert_resource(Midi {
+            output_handle: handle,
+        })
         .run();
 }
 
@@ -18,7 +47,9 @@ fn setup_camera(mut commands: Commands) {
 }
 
 #[derive(Component)]
-struct Ball;
+struct Ball {
+    octave: i32,
+}
 
 fn spawn_ball(
     mut commands: Commands,
@@ -26,18 +57,40 @@ fn spawn_ball(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     commands.spawn((
-        Ball,
+        Ball { octave: 2 },
+        RigidBody::Dynamic,
+        Restitution::new(1.0),
+        Collider::circle(25.0),
+        Transform::from_xyz(0.0, 0.0, 0.0),
+        Mesh2d(meshes.add(Circle::new(25.0))),
+        MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::linear_rgb(0.1, 0.3, 0.8)))),
+    ));
+
+    commands.spawn((
+        Ball { octave: 3 },
         RigidBody::Dynamic,
         Restitution::new(1.0),
         Collider::circle(15.0),
-        Transform::from_xyz(0.0, 0.0, 0.0),
+        Transform::from_xyz(-100.0, 0.0, 0.0),
         Mesh2d(meshes.add(Circle::new(15.0))),
-        MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::linear_rgb(0.8, 0.8, 0.8)))),
+        MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::linear_rgb(0.8, 0.7, 0.3)))),
+    ));
+
+    commands.spawn((
+        Ball { octave: 4 },
+        RigidBody::Dynamic,
+        Restitution::new(1.0),
+        Collider::circle(10.0),
+        Transform::from_xyz(100.0, 0.0, 0.0),
+        Mesh2d(meshes.add(Circle::new(10.0))),
+        MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::linear_rgb(0.8, 0.3, 0.3)))),
     ));
 }
 
 #[derive(Component)]
-struct Pad;
+struct Pad {
+    note: midi::Note,
+}
 
 fn spawn_pad_box(
     centre: Vec2,
@@ -54,11 +107,14 @@ fn spawn_pad_box(
         .spawn((
             RigidBody::Kinematic,
             AngularVelocity(1.5),
-            Transform::from_xyz(centre.x, centre.y, 0.0),
+            Transform::from_xyz(centre.x, centre.y, 0.0).with_rotation(Quat::from_rotation_z(0.2)),
+            Visibility::default(),
         ))
         .with_children(|commands| {
             commands.spawn((
-                Pad,
+                Pad {
+                    note: midi::Note::C,
+                },
                 restitution,
                 Collider::rectangle(size, pad_thickness),
                 Mesh2d(meshes.add(Rectangle::new(size, pad_thickness))),
@@ -71,7 +127,9 @@ fn spawn_pad_box(
             ));
 
             commands.spawn((
-                Pad,
+                Pad {
+                    note: midi::Note::E,
+                },
                 restitution,
                 Collider::rectangle(size, 5.0),
                 Mesh2d(meshes.add(Rectangle::new(size, 5.0))),
@@ -80,11 +138,14 @@ fn spawn_pad_box(
                     centre.x,
                     centre.y - (size / 2.0) + (pad_thickness / 2.0),
                     0.0,
-                ),
+                )
+                .with_rotation(Quat::from_rotation_z(-PI)),
             ));
 
             commands.spawn((
-                Pad,
+                Pad {
+                    note: midi::Note::G,
+                },
                 restitution,
                 Collider::rectangle(size, 5.0),
                 Mesh2d(meshes.add(Rectangle::new(size, 5.0))),
@@ -94,11 +155,13 @@ fn spawn_pad_box(
                     centre.y,
                     0.0,
                 )
-                .with_rotation(Quat::from_rotation_z(PI / 2.0)),
+                .with_rotation(Quat::from_rotation_z(-PI / 2.0)),
             ));
 
             commands.spawn((
-                Pad,
+                Pad {
+                    note: midi::Note::A,
+                },
                 restitution,
                 Collider::rectangle(size, 5.0),
                 Mesh2d(meshes.add(Rectangle::new(size, 5.0))),
@@ -123,19 +186,42 @@ fn setup_pads(
     );
 }
 
-fn trigger_note_from_collision(pad: &Pad) {
-    //Send midi note...
+//Update
+fn handle_pad_collision(
+    collision: &Contacts,
+    pad: &Pad,
+    ball: &Ball,
+    ball_velocity: &LinearVelocity,
+    mut midi_output: &mut Option<MidiOutputConnection>,
+) {
+    if collision.collision_started() {
+        midi::note_on(
+            pad.note,
+            ball.octave,
+            midi::to_velocity(ball_velocity.length()),
+            &mut midi_output,
+        );
+    }
+    if collision.collision_stopped() {
+        midi::note_off(pad.note, 4, &mut midi_output);
+    }
 }
 
-//Update
-fn handle_pad_collisions(mut collisions: EventReader<Collision>, pads: Query<&Pad>) {
+fn handle_pad_collisions(
+    mut collisions: EventReader<Collision>,
+    pads: Query<&Pad>,
+    balls: Query<(&Ball, &LinearVelocity)>,
+    mut midi: ResMut<Midi>,
+) {
     for Collision(collision) in collisions.read() {
-        if collision.collision_started() {
-            if let Ok(pad) = pads.get(collision.entity1) {
-                trigger_note_from_collision(pad);
+        if let Ok(pad) = pads.get(collision.entity1) {
+            if let Ok((ball, velocity)) = balls.get(collision.entity2) {
+                handle_pad_collision(collision, pad, ball, velocity, &mut midi.output_handle);
             }
-            if let Ok(pad) = pads.get(collision.entity2) {
-                trigger_note_from_collision(pad);
+        }
+        if let Ok(pad) = pads.get(collision.entity2) {
+            if let Ok((ball, velocity)) = balls.get(collision.entity1) {
+                handle_pad_collision(collision, pad, ball, velocity, &mut midi.output_handle);
             }
         }
     }
---
+++ src/midi.rs
@@ -0,0 +1,87 @@
+use avian2d::parry::na::clamp;
+use midir::MidiOutputConnection;
+
+const NOTE_ON_MSG: u8 = 0x90;
+const NOTE_OFF_MSG: u8 = 0x80;
+
+#[derive(Clone, Copy)]
+pub enum Note {
+    C,
+    E,
+    G,
+    A,
+}
+
+pub const C3: u8 = 0x30;
+pub const E3: u8 = 0x34;
+pub const G3: u8 = 0x37;
+pub const A3: u8 = 0x39;
+
+pub const C4: u8 = 0x3c;
+pub const E4: u8 = 0x40;
+pub const G4: u8 = 0x43;
+pub const A4: u8 = 0x45;
+
+pub const C5: u8 = 0x48;
+pub const E5: u8 = 0x4C;
+pub const G5: u8 = 0x4F;
+pub const A5: u8 = 0x51;
+
+fn to_note_value(note: Note, octave: i32) -> u8 {
+    if octave <= 3 {
+        return match note {
+            Note::C => C3,
+            Note::E => E3,
+            Note::G => G3,
+            Note::A => A3,
+        };
+    }
+
+    if octave == 4 {
+        return match note {
+            Note::C => C4,
+            Note::E => E4,
+            Note::G => G4,
+            Note::A => A4,
+        };
+    }
+
+    match note {
+        Note::C => C5,
+        Note::E => E5,
+        Note::G => G5,
+        Note::A => A5,
+    }
+}
+
+pub fn note_on(
+    note: Note,
+    octave: i32,
+    velocity: u8,
+    midi_output: &mut Option<MidiOutputConnection>,
+) {
+    if let Some(midi_output) = midi_output {
+        println!("Midi Velocity: {}", velocity);
+        let _ = midi_output.send(&[NOTE_ON_MSG, to_note_value(note, octave), velocity]);
+    }
+}
+
+pub fn note_off(note: Note, octave: i32, midi_output: &mut Option<MidiOutputConnection>) {
+    const VELOCITY: u8 = 0x7F;
+
+    if let Some(midi_output) = midi_output {
+        let _ = midi_output.send(&[NOTE_OFF_MSG, to_note_value(note, octave), VELOCITY]);
+    }
+}
+
+pub fn to_velocity(speed: f32) -> u8 {
+    const MAX_SPEED: f32 = 750.0;
+    const MIN_SPEED: f32 = 50.0;
+
+    let scaled0to1 = (clamp(speed, MIN_SPEED, MAX_SPEED) - MIN_SPEED) / (MAX_SPEED - MIN_SPEED);
+
+    const MAX_MIDI_VEL: u8 = 0x7F;
+    let scaled = MAX_MIDI_VEL as f32 * scaled0to1;
+
+    scaled as u8
+}
